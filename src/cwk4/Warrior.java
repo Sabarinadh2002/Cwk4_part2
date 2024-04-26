ceb570e1c493c987bd7502aaa6ceb9d05ecf4586
macbook <mgf6767@gmail.com>
2024-04-26 16:53:35 +0100

    updated the warrior class with getWeaponType constructure

--- src/cwk4/Warrior.java
+++ src/cwk4/Warrior.java
@@ -9,7 +9,18 @@ import java.io.Serializable;
  * @author (your name)
  * @version (a version number or a date)
  */
+public class Warrior extends Champion implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String weaponType;
 
+    public Warrior(String name, int skillLevel, int entryFee, String weaponType) {
+        super(name, skillLevel, entryFee, "Warrior");
+        this.weaponType = weaponType;
+    }
+
+    public String getWeaponType() {
+        return weaponType;
+    }
 
     // Add any other warrior-specific methods here
 }
