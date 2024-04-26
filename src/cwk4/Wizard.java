1de6df4c805698918d52d89662c3f8446d76c961
mohamed <Mohamedhass928@gmail.com>
2024-04-26 10:55:40 +0100

    updated wizard clas{implements serializable}

--- src/cwk4/Wizard.java
+++ src/cwk4/Wizard.java
@@ -9,7 +9,16 @@ import java.io.Serializable;
  * @author (your name)
  * @version (a version number or a date)
  */
+public class Wizard extends Champion implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String spellSpeciality;
+    private boolean isNecromancer;
 
+    public Wizard(String name, int skillLevel, int entryFee, boolean isNecromancer, String spellSpeciality) {
+        super(name, skillLevel, entryFee, "Wizard");
+        this.spellSpeciality = spellSpeciality;
+        this.isNecromancer = isNecromancer;
+    }
 
     public String getSpellSpeciality() {
         return spellSpeciality;
